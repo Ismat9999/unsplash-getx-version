12ed73c6cfabfddfdd8cbf8467322eee9aaba111
Ismat9999 <redgoo123@gmail.com>
2025-01-06 23:38:00 +0500

    getx read

--- lib/core/config/root_binding.dart
+++ lib/core/config/root_binding.dart
@@ -1,12 +1,14 @@
 
 import 'package:get/get.dart';
 import 'package:unsplashdemo/presantation/controller/collection_controller.dart';
+import 'package:unsplashdemo/presantation/controller/home_controller.dart';
 import 'package:unsplashdemo/presantation/controller/photos_controller.dart';
 import 'package:unsplashdemo/presantation/controller/sch_controller.dart';
 
 class RootBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(()=>HomeController(),fenix: true);
     Get.lazyPut(()=>SchController(),fenix: true);
     Get.lazyPut(()=>CollectionController(),fenix: true);
     Get.lazyPut(()=>PhotosController(),fenix: true);
